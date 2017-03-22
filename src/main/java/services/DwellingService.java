9121f5d423be81a32d5713d4e87af5ea6445bac7
Emile Bigot <Emile Bigot>
2017-03-22 22:13:26 +0100

    initial commit

---
+++ src/main/java/services/DwellingService.java
@@ -0,0 +1,20 @@
+package services;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/dwellingservice")
+public class DwellingService {
+
+    @GET
+    @Path("/createdwelling")
+    @Consumes("application/json")
+    public Response generateBusinessRule(String JSONArray) {
+        System.out.println("Generate call with: \n" + JSONArray.toString());
+
+        System.out.println("Generated output: \n" + JSONArray.toString());
+        return Response.status(200).entity("Bij mij werkt hij lekker wel!").build();
+    }
+}
